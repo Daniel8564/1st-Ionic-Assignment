d0ae2bdc2b46b45a14593976d286872cbf83dde6
Daniel8564 <daniel.bench85@gmail.com>
2020-03-31 15:58:03 +1100

    First revision of assignment 1

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,35 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonFab, IonFabButton, IonIcon, IonFabList } from '@ionic/react';
 import React from 'react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
+
+import { add, settings, share, person, arrowForwardCircle, arrowBackCircle, arrowUpCircle, logoVimeo, logoFacebook, logoInstagram, logoTwitter } from 'ionicons/icons';
 
 const Home: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>Daniel's Ionic Project</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
+      <IonContent className="ion-padding">
+	  	The world is your oyster.
+		<p>
+		  if you get lost, the {' '}
+		  <a 
+		    target="_blank"
+			rel="nooper"
+			href="https://ionicframework.com/docs">
+			 docs </a> {' '}
+			 will be your guide.
+		</p>
+			<IonFab vertical="bottom" horizontal="end" slot="fixed">
+    			<IonFabButton>
+					<IonIcon icon={add} />
+    			</IonFabButton>
+  			</IonFab>
+        
         <ExploreContainer />
       </IonContent>
     </IonPage>
